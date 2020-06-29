0bb02916f1da91dd00ca52330be43ce60257a671
Patrice Gill <pgill30@fb.com>
2020-06-29 19:11:40 -0400

    Added User and Tweet model classes

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,24 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    public String name;
+    public String screenName; //handle
+    public String profileImageURl;
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageURl = jsonObject.getString("profile_image_url");
+        return user;
+    }
+
+
+}
