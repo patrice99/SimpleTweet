e52e071078d1041bbbb92551193eb5fe28ce838a
Patrice Gill <pgill30@fb.com>
2020-06-30 18:05:14 -0400

    Navigating to ComposeActivity from TimelineActivity

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -87,7 +88,11 @@ public class TimelineActivity extends AppCompatActivity {
             //Compose icon has been selected
             Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
             //Navigate to compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+            return true;
+
         }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
