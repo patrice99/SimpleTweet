c207523feb145f994aa7f32e2c74e14eb51b7dfd
Patrice Gill <pgill30@fb.com>
2020-06-30 18:01:38 -0400

    Added a Compose activity and compose layout file

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,45 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    EditText etCompose;
+    Button btnTweet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        //Set click listener button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Make an API call to twitter to publish tweet.
+                String tweetContent = etCompose.getText().toString();
+                //if tweet content is empty, retry
+                if (tweetContent.isEmpty()) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //if tweet is too long, then retry
+                if(tweetContent.length() > 140){
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet exceeds 140 characters", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
