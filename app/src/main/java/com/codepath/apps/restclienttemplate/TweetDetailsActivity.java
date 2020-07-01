13618ce5d021abcaa99a9138982acb65ecefa4d7
Patrice Gill <pgill30@fb.com>
2020-07-01 18:08:43 -0400

    Modified Tweet Details XML and imported twitter icons

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetDetailsActivity.java
@@ -2,7 +2,10 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TweetDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class TweetDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_details);
+
+        TextView tvScreenName;
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvTimeStamp;
+        ImageView ivMedia;
+
+        tvScreenName = findViewById(R.id.tvScreenName);
+        tvBody = findViewById(R.id.tvBody);
+        tvTimeStamp = findViewById(R.id.tvTimeStamp);
+        ivProfileImage = findViewById(R.id.ivProfileImage);
+        ivMedia = findViewById(R.id.ivMedia);
+
+        
+
     }
+
 }
