e3c14e6abb6c73391b391545873a77c421c58234
Patrice Gill <pgill30@fb.com>
2020-07-01 15:51:04 -0400

    Added Material Text Field layout for character counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,7 +31,7 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
-        etCompose = findViewById(R.id.etCompose);
+        etCompose = findViewById(R.id.tfCompose);
         btnTweet = findViewById(R.id.btnTweet);
 
         client = TwitterApp.getRestClient(this);
