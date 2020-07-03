19b6d97b485143d6e8e17580ff34911b298a27ed
Patrice Gill <pgill30@fb.com>
2020-07-03 00:25:47 -0400

    Sent an API Request to retrieve more tweets and populated into recycler view by adapter

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -92,6 +92,17 @@ public class TwitterClient extends OAuthBaseClient {
 		client.post(apiUrl, params, "", handler);
 	}
 
+	public void getNextPageOfTweets(JsonHttpResponseHandler handler, long maxId) {
+		String apiUrl = getApiUrl("statuses/home_timeline.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("count", 25);
+		params.put("max_id", maxId);
+
+		client.get(apiUrl, params, handler);
+	}
+
+
 
 
 
