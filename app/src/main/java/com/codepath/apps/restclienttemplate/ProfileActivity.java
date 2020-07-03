66ff55aa7b98b8e6027750c1472d50c9759ba9d8
Patrice Gill <pgill30@fb.com>
2020-07-03 01:38:14 -0400

    Binded Views on ProfilePic Activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -3,10 +3,16 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.databinding.ActivityProfileBinding;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.parceler.Parcels;
 
 public class ProfileActivity extends AppCompatActivity {
+    Tweet tweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,17 @@ public class ProfileActivity extends AppCompatActivity {
 
         ActivityProfileBinding binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        tweet = Parcels.unwrap(getIntent().getParcelableExtra("tweet"));
+
+        binding.tvScreenName.setText("@" + tweet.user.screenName);
+        binding.tvName.setText(tweet.user.name);
+        Glide.with(this).load(tweet.user.profileImageURl).circleCrop().into(binding.ivProfileImage);
+        binding.tvBio.setText(tweet.user.bio);
+        binding.tvFollowers.setText("Followers: " + tweet.user.followers);
+        binding.tvFollowing.setText("Following: " + tweet.user.following);
+
+
 
 
 
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -13,6 +13,9 @@ public class User {
     public String name;
     public String screenName; //handle
     public String profileImageURl;
+    public String bio;
+    public String followers;
+    public String following;
 
     //empty constructor for parceler
     public User() {}
@@ -22,6 +25,9 @@ public class User {
         user.name = jsonObject.getString("name");
         user.screenName = jsonObject.getString("screen_name");
         user.profileImageURl = jsonObject.getString("profile_image_url_https");
+        user.bio = jsonObject.getString("description");
+        user.followers = jsonObject.getString("followers_count");
+        user.following = jsonObject.getString("friends_count");
         return user;
     }
 
